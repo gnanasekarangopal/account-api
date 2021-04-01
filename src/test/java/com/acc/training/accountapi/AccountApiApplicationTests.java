cf9067f0289bcebef972c82b6e4bf9e0c41947de
gnanasekarangopal <81674822+gnanasekarangopal@users.noreply.github.com>
2021-04-01 14:29:50 +0530

    build

---
+++ src/test/java/com/acc/training/accountapi/AccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acc.training.accountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
